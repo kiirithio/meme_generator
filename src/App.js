880aa071b17ec1e7b4cfd2aad8361d44abeb32bb
kiirithio <kiirithiogeorge101@gmail.com>
2023-01-17 12:40:20 +0300

    2nd commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from 'D:/React Projects/memegenerator/src/components/Header';
+import MemeGenerator from 'D:/React Projects/memegenerator/src/components/MemeGenerator.js';
+
+
+function App() {
+  return (
+    <div>
+      <Header/>
+      <MemeGenerator/>
+    </div>
+  );
+}
+
+export default App;
